d75a69d2217c44d54461ff382fa031964e09e23a
aurelia ritter <aureliaritter@gmail.com>
2023-06-06 20:59:04 +0400

    routes de sections

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  get 'sections/index'
-  get 'sections/show'
   devise_for :users
-  root to: "pages#home"
+  root to: "sections#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :sections, only: %i[index]
+  get "sections/:title", to: "sections#show", as: :section
   # Defines the root path route ("/")
   # root "articles#index"
 end
