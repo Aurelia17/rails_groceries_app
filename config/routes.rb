53d6b0a5fa375fa150f0adbe504f779c292351f6
aurelia ritter <aureliaritter@gmail.com>
2023-06-08 19:11:19 +0400

    sections#show :controller et base pour view

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: "sections#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :sections, only: %i[index show] do
-    resources :products, only: %i[index show] do
+    resources :products, only: %i[show] do
       resources :ratings, only: %i[index new]
     end
   end
