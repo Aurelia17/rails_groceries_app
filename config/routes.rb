cea2bed79e459ea767c6762dfdb824f2794efba4
aurelia ritter <aureliaritter@gmail.com>
2023-06-08 13:46:16 +0400

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,11 @@
 Rails.application.routes.draw do
-  get 'products/show'
   devise_for :users
   root to: "sections#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :sections, only: %i[index]
-  get "sections/:title", to: "sections#show", as: :section
-  get "checkout_cart", to: "checkout_cart#show", as: :checkout_cart
-  resources :checkout_cart, only: %i[new]
-  post "checkout_cart", to: "checkout_cart#create"
-  get "checkout_cart/edit", to: "checkout_cart#edit", as: :edit_checkout_cart
-  put "checkout_cart", to: "checkout_cart#update"
-  patch "checkout_cart", to: "checkout_cart#update"
-  delete "checkout_cart", to: "checkout_cart#update"
+  resources :sections, only: %i[index show] do
+    resources :products, only: %i[index show]
+  end
+  resources :checkout_cart, only: %i[show new create edit update delete]
   # Defines the root path route ("/")
   # root "articles#index"
 end
