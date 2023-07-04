69b3a14238a50d32b320dc1fe9a1c53a613529cc
tejas boodoo <shiveshboodoo391@gmail.com>
2023-07-04 19:34:30 +0400

    map completed

--- app/javascript/controllers/contact_map_controller.js
+++ app/javascript/controllers/contact_map_controller.js
@@ -3,16 +3,23 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="contact-map"
 export default class extends Controller {
   static values = {
-    apiKey: String,
-    markers: Array
+    apiKey: String
   }
 
   connect() {
+
+    console.log("hello 123")
     mapboxgl.accessToken = this.apiKeyValue
 
     this.map = new mapboxgl.Map({
       container: this.element,
-      style: "mapbox://styles/mapbox/streets-v10"
+      style: "mapbox://styles/mapbox/streets-v10",
+      center: [57.5522, -20.3000],
+      zoom: 12
     })
+    new mapboxgl.Marker()
+    .setLngLat([57.5208, -20.2175 ])
+    .addTo(this.map)
+
   }
 }
